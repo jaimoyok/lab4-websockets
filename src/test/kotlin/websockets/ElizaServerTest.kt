7e4197731defd6746ae77a1bfaffb12b1e708d92
jaimoyok <jaimeyoldi2@gmail.com>
2021-12-03 18:51:46 +0100

    complete ElizaServerTest

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -46,8 +46,8 @@ class ElizaServerTest {
         val client = ElizaOnOpenMessageHandlerToComplete(list, latch)
         container.connectToServer(client, URI("ws://localhost:$port/eliza"))
         latch.await()
-        // assertEquals(XXX, list.size) COMPLETE ME
-        // assertEquals(XXX, list[XXX]) COMPLETE ME
+        assert(list.size >= 4)
+        assertEquals("Can you think of a specific example?", list[3])
     }
 
 }
@@ -68,8 +68,8 @@ class ElizaOnOpenMessageHandlerToComplete(private val list: MutableList<String>,
     fun onMessage(message: String, session: Session)  {
         list.add(message)
         latch.countDown()
-        // if (COMPLETE ME) {
-        //    COMPLETE ME
-        // }
+        if(latch.count == 1L){
+            session.basicRemote.sendText("always")
+        }
     }
 }
